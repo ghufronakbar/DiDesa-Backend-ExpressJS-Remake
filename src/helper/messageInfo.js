5f27cc49c8511ddaaece9ca9deb6c979630eea08
ghufron akbar <alan.040219.gam@gmail.com>
2024-07-29 17:30:14 +0700

    little fix

--- src/helper/messageInfo.js
+++ src/helper/messageInfo.js
@@ -2,7 +2,7 @@ const formatDate = require('../utils/formatDate');
 const formatTime = require('../utils/formatTime');
 
 const messageApproveUmkm = (umkm, jenis, pemilik, date) => {
-    return `Selamat, UMKM Anda telah berhasil disetujui untuk tampil pada aplikasi Di Desa pada: \nTanggal: ${formatDate(date)}\nPukul: ${formatTime(date)}.\n\n*Data UMKM*:\nNama: ${umkm}\nJenis: ${jenis}\nPemilik: ${pemilik}\n\nHarap simpan informasi ini dengan aman. Terima kasih telah menggunakan aplikasi Di Desa.`;
+    return `Selamat, UMKM Anda telah berhasil disetujui untuk tampil pada aplikasi Di Desa pada: \nTanggal: ${formatDate(date)}\nPukul: ${formatTime(date)}\n\n*Data UMKM*:\nNama: ${umkm}\nJenis: ${jenis}\nPemilik: ${pemilik}\n\nHarap simpan informasi ini dengan aman. Terima kasih telah menggunakan aplikasi Di Desa.`;
 }
 
 const messageNotApproveUmkm = (umkm, jenis, pemilik) => {
