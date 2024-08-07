b26706e905c9c53a8f7a6f571282f5a497335f50
ghufron akbar <alan.040219.gam@gmail.com>
2024-08-07 22:59:53 +0700

    add Cache

--- src/useCase/user/informasiDesa/index.js
+++ src/useCase/user/informasiDesa/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { getInformasiController } = require('./informasiDesa.controller');
 const { userCheck } = require('../../../middleware/userCheck');
+const routeCache = require('../../../utils/routeCache')
 
-router.get('/', userCheck, getInformasiController)
+router.get('/', routeCache(300), userCheck, getInformasiController)
 
 module.exports = router
