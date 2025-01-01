49d2f90f6d0a44bb5d2cd40a1ee6aa4cc8ecf324
ghufron akbar <alan.040219.gam@gmail.com>
2025-01-01 19:11:23 +0700

    update profile pic

--- src/useCase/user/informasiDesa/informasiDesa.service.js
+++ src/useCase/user/informasiDesa/informasiDesa.service.js
@@ -1,8 +1,12 @@
+const { PROFILE_DEFAULT } = require('../../../constant/imageDefault')
 const { getInformasiDesa, getPengurusDesa } = require('./informasiDesa.repository')
 
 const getInformasiService = async () => {
     const informasiDesa = await getInformasiDesa()
     const pengurusDesa = await getPengurusDesa()
+    for (const pengurus of pengurusDesa) {
+        pengurus.warga.foto == null ? pengurus.warga.foto = PROFILE_DEFAULT : pengurus.warga.foto
+    }
     return { informasiDesa, pengurusDesa }
 }
 
