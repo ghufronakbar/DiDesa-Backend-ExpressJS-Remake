4c9f1acce0387c4b73ebd71e1e5b8994572124c9
lanstheprodigy <alan.040219.gam@gmail.com>
2025-11-15 15:10:07 +0700

    add rss schema

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(cors());
 
+app.use('/api/ping', require('./routes/internalPing'));
 app.use('/api/admin', require('./routes/admin'));
 app.use('/api/user',require('./routes/user'));
 
